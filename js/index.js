7451a9a5899387b0dbc3033854a8daf4b2fab7ff
owenpo <33616236+uragunz16@users.noreply.github.com>
2025-06-07 09:17:19 +0800

    refactor dashboard link

--- js/index.js
+++ js/index.js
@@ -4,6 +4,17 @@ document.querySelectorAll('.skill-progress').forEach(bar => {
 
 // 
 function openProject(projectName) {
-    // Redirect to the new page based on the project name
-    window.location.href = projectName + ".html";
-}
+    // // Redirect to the new page based on the project name
+    // window.location.href = projectName + ".html";
+    // get the content
+    let projectContainer = document.getElementById("dashboard");
+
+    // Fetch the project content dynamically
+    fetch(projectName + ".html")
+    .then(response => response.text())
+    .then(data => {
+      projectContainer.innerHTML = data;
+    })
+    .catch(error => console.error("Error loading project", error));
+};
+
