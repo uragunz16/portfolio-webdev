e9537564a5c64f8cf004f52dbc0d82d72c2e01c2
owenpo <33616236+uragunz16@users.noreply.github.com>
2025-06-09 19:15:14 +0800

    add anchor to link and temporary disable to js link

--- js/index.js
+++ js/index.js
@@ -2,19 +2,29 @@ document.querySelectorAll('.skill-progress').forEach(bar => {
   bar.style.width = bar.getAttribute('data-width');
 });
 
-// 
+/* 
 function openProject(projectName) {
-    // // Redirect to the new page based on the project name
+    // Redirect to the new page based on the project name
     // window.location.href = projectName + ".html";
-    // get the content
-    let projectContainer = document.getElementById("dashboard");
 
-    // Fetch the project content dynamically
-    fetch(projectName + ".html")
-    .then(response => response.text())
-    .then(data => {
-      projectContainer.innerHTML = data;
-    })
-    .catch(error => console.error("Error loading project", error));
-};
+  // const projectURL =  document.getElementById(projectName);
+  // if (projectURL) {
+  //   window.open(projectName + ".html", "_blank");
+  // }
 
+  window.open(projectName + ".html", "_blank");
+}
+*/ 
+
+document.querySelectorAll('.project-card').forEach(card => {
+    card.addEventListener('click', function() {
+        const projectName = this.getAttribute('onclick').match(/'([^']+)'/)[1]; 
+        if (projectName) {
+            window.open(projectName + ".html", "_blank"); // Opens in a new tab
+        }
+    });
+});
+
+// function openProject(projectName) {
+//   console.log(projectName, "openProject is clicked");
+// }
